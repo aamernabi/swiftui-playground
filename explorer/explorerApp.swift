f8eb3e4bfa729d03b543bcd093ab1483f6374bae
Aamer Paul <aamir.paul@lelafe.com>
2022-12-25 16:56:17 +0530

    Initial Commit

---
+++ explorer/explorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  explorerApp.swift
+//  explorer
+//
+//  Created by Lelafe on 25/12/22.
+//
+
+import SwiftUI
+
+@main
+struct explorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
