f041ee152d03f94465cbac7cef9617146ae1791e
Aamer Paul <aamir.paul@lelafe.com>
2023-01-20 11:54:27 +0530

    changes content view group

---
+++ explorer/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  explorer
+//
+//  Created by Lelafe on 25/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
